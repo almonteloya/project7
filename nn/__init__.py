69bfaf3908d26d983ea0726da1f866557b699b2b
almonteloya <ana.almonteloya@gmail.com>
2022-03-10 17:34:25 -0800

    update innit

--- nn/__init__.py
+++ nn/__init__.py
@@ -3,8 +3,8 @@ BMI203
 Project 7: Neural Network
 """
 
-from .io import (read_text_file, read_fasta_file)
-from .nn import NeuralNetwork
-from .preprocess import (one_hot_encode_seqs, sample_seqs)
+from nn.io import (read_text_file, read_fasta_file)
+from nn.nn import NeuralNetwork
+from nn.preprocess import (one_hot_encode_seqs, sample_seqs)
 
 __version__ = '0.1.0'
